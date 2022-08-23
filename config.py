d3bb1900d1a4b43c1193501f9e7be78c90c039c9
Shopiley <fadekemi.adebayo@pau.edu.ng>
2022-08-23 21:09:18 +0100

    setup postgres database on heroku

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ DEBUG = True
 
 # TODO IMPLEMENT DATABASE URL
 # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/fyyur'
-SQLALCHEMY_DATABASE_URI = 'postgres://hmazppnfepzjsb:73afc90bd46be50812a1d509a4611e8daf61e19bb3286978dbcdf7b846192bc3@ec2-34-199-68-114.compute-1.amazonaws.com:5432/d16b9ec5afq4af'
+SQLALCHEMY_DATABASE_URI = 'postgresql://hmazppnfepzjsb:73afc90bd46be50812a1d509a4611e8daf61e19bb3286978dbcdf7b846192bc3@ec2-34-199-68-114.compute-1.amazonaws.com:5432/d16b9ec5afq4af'
 SQLALCHEMY_TRACK_MODIFICATIONS = 'False' 
